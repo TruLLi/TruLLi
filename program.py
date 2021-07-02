e25cfd46fe22fb031ff616c77a4477f68e418ac8
Denis Truhlaz <denis.truhlaz@immowelt.de>
2021-07-02 12:40:53 +0200

    WIP function for saving result img/video

--- program.py
+++ program.py
@@ -1,36 +1,51 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode as qrCodeDecode
+import os
 import time
 
-
-capture = cv2.VideoCapture('samples.data/test1.mp4') #kada zelimo video
-writer = None
+capture = cv2.VideoCapture('samples.data/test3.mp4') #kada zelimo video
 #capture = cv2.VideoCapture(0)  #kada zelimo live kameru
-#capture = cv2.imread('nesto.jpg') #TODO za slike moram napravit
+#capture = cv2.imread('samples.data/test_picture_3.jpeg') #TODO za slike moram napravit
+writer = None
 widthHeightTarget = 320
 confThreshHold = 0.5
 nmsThreshHold = 0.3 #sto je broj manji to ce threshhold biti agresivniji i imat cemo manji broj bboxova unutar bboxova
+colors = [tuple(255 * np.random.rand(3)) for i in range(5)]
+
+#inizijalizacija imena koje cemo koristit u prepoznavanju
+classesFile = 'model.data/custom.names'
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+#inizijalizacija modela
+modelConfiguration = 'model.data/yolov4-helmet.cfg'
+modelWeights = 'model.data/yolov4-helmet-detection.weights'
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV) #tu mozemo odabrat i CUDA ako imamo graficku i drivere isntalirane
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU) #isto tako mozemo i ovdje odabrati CUDA ako imamo graficku, graficka puno brze obraduje sliku nego CPU
 
 frame_width = int(capture.get(3))
 frame_height = int(capture.get(4))
 
 size = (frame_width, frame_height)
 
-classesFile = 'model.data/custom.names'
-classNames = []
-with open(classesFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
 
-modelConfiguration = 'model.data/yolov4-helmet.cfg'
-modelWeights = 'model.data/yolov4-helmet-detection.weights'
+#inizijalizacija writera koji ce spremit vrijednost u outputs folder ako loadamo video ili live kameru kao ulaz
+#funkcija koja sprema rezultat u outputs folder
+def saveResult(img, videoCapture):
+    if videoCapture.isOpened():
+        frame_width = int(videoCapture.get(3))
+        frame_height = int(videoCapture.get(4))
+        size = (frame_width, frame_height)
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        writer = cv2.VideoWriter('outputs/result.avi', fourcc, 20, size)
+        writer.write(img)
+    else:
+        cv2.imwrite('outputs/resultImg.jpg', img)
 
-colors = [tuple(255 * np.random.rand(3)) for i in range(5)]
-
-net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV) #tu mozemo odabrat i CUDA ako imamo graficku i drivere isntalirane
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU) #isto tako mozemo i ovdje odabrati CUDA ako imamo graficku, graficka puno brze obraduje sliku nego CPU
-
+#funkcija za citanje barkodova na slici / video zapisu / live kameri
 def decodeBarcodes(img):
     for barcode in qrCodeDecode(img):
         myData = barcode.data.decode('utf-8')
@@ -40,6 +55,7 @@ def decodeBarcodes(img):
         textPoints = barcode.rect  # ovo su pointovi za text kada procita barcode
         cv2.putText(img, myData, (textPoints[0], textPoints[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 217, 255), 2)
 
+#glavna funkcija koja trazi objekte na slici / video zapisu / live kameri
 def findObjects(outputs, img):
     hT, wT, cT = img.shape #hT - height, wT- width, cT - channels
     bbox = [] #bounding box
@@ -61,23 +77,15 @@ def findObjects(outputs, img):
     indexs = cv2.dnn.NMSBoxes(bbox, confValue, confThreshHold, nmsThreshHold)
     #print(indexs)
     for color, i in zip(colors, indexs):
-        #data = barcode.data.decode('utf-8')
-        #points = np.array([barcode.polygon], np.int32)
-        #points = points.reshape((-1, 1, 2))
         i = i[0]
         box = bbox[i]
         x, y, w, h = box[0], box[1], box[2], box[3]
         #corner points x+w i y+h
         cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
         cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confValue[i] * 100)}%', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-        decodeBarcodes(img)
-        #cv2.polylines(img, [points], True, color, 4)
-        #textPoints = barcode.rect
-        #cv2.putText(img, data, (textPoints[0], textPoints[1]), cv2.FONT_HERSHEY_PLAIN, 0.9, color, 2)
-
+        decodeBarcodes(img) #funkcija za citanje barkodova ce se pozvati samo u slucaju ako ima uspjesnog pronalaska objekata koje trazimo
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 writer = cv2.VideoWriter('outputs/result.avi', fourcc, 20, size)
-
 while True:
     success, img = capture.read()
     blob = cv2.dnn.blobFromImage(img, 1/255, (widthHeightTarget, widthHeightTarget), [0, 0, 0], 1, crop=False) #ovo ce sliku pretvorit u blob
@@ -100,7 +108,12 @@ while True:
     # rezultat ovoga je matrix koji ima 300[0], 1200[1], 4800[2] redova i 8 razlicith stupaca
     # 300/1200/4800 je broj of bounding boxes , dok broj 8 znaci 1-center x, 2-center y, 3-w, 4-h, 5-confidance da se ono sto trazimo nalazi tamo, a ostale 3 vrijednosti su predictions probabilites ostalih klasa
     #print(outputs[0][0]) primjer outputa jednog elementa
+
     findObjects(outputs, img)
+    #saveResult(img, capture)
+    #ako nema outputs, foldera napravi ga
+    if not os.path.exists('outputs'):
+        os.mkdir('outputs')
 
     if success == True:
         writer.write(img)
